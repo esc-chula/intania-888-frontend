73f76669b64d65694830c820ea5fe897e1868f5d
neennera <neennera@gmail.com>
2024-10-03 16:12:33 +0700

    chore: justify start for nav

--- src/app/coins/page.tsx
+++ src/app/coins/page.tsx
@@ -1,19 +1,12 @@
-import { EmptyState } from "@/components/EmptyState";
 import { Header } from "@/components/Header";
 import { Navbar } from "@/components/Navbar";
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center space-y-4 h-screen">
+    <div className="flex flex-col items-center justify-start space-y-4 h-screen">
       <div className="relative m-0 p-0 top-0 flex flex-col w-screen">
         <Header />
         <Navbar pagenow="coins" />
       </div>
-      <EmptyState
-        texts={[
-          "หากต้องการทายผล ทำตามขั้นตอนดังนี้",
-          "แมตช์ > เลือกแมตช์ที่ต้องการทาย > เพิ่งลงสลิป",
-        ]}
-      />
     </div>
   );
 }
