a44ee57c952cb68fb69a1ac783629dd5b90f05b3
neennera <neennera@gmail.com>
2024-10-03 14:58:21 +0700

    fix: hover white bg

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,21 +7,21 @@ export const Navbar = () => {
           <Trophy />
           <p>แมตช์</p>
         </div>
-        <div className="    :h-1  w-full h-0 bg-base-gold transition-all absolute bottom-0"></div>
+        <div className="group-hover:h-1  w-full h-0 group-hover:bg-white transition-all absolute bottom-0"></div>
       </div>
       <div className="w-1/4 h-full items-center justify-center group relative">
         <div className="flex flex-row space-x-2 h-full items-center justify-center">
           <ReceiptText />
           <p>สลิป</p>
         </div>
-        <div className="group-hover:h-1  w-full h-0 bg-base-gold transition-all absolute bottom-0"></div>
+        <div className="group-hover:h-1  w-full h-0 group-hover:bg-white transition-all absolute bottom-0"></div>
       </div>
       <div className="w-1/4 h-full items-center justify-center group relative">
         <div className="flex flex-row space-x-2 h-full items-center justify-center">
           <Joystick />
           <p>อีเวนต์</p>
         </div>
-        <div className="group-hover:h-1  w-full h-0 bg-base-gold transition-all absolute bottom-0"></div>
+        <div className="group-hover:h-1  w-full h-0 group-hover:bg-white transition-all absolute bottom-0"></div>
       </div>
 
       <div className="w-1/4 h-full items-center justify-center group relative">
@@ -29,7 +29,7 @@ export const Navbar = () => {
           <p>100,000</p>
           <Coins color="yellow" />
         </div>
-        <div className="group-hover:h-1  w-full h-0 bg-base-gold transition-all absolute bottom-0"></div>
+        <div className="group-hover:h-1  w-full h-0 group-hover:bg-white transition-all absolute bottom-0"></div>
       </div>
     </div>
   );
