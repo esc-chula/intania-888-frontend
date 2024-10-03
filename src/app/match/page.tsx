c591ee97c3530a2c9cb1564844468ab103a0cb18
neennera <neennera@gmail.com>
2024-10-03 18:50:11 +0700

    add: filter main

--- src/app/match/page.tsx
+++ src/app/match/page.tsx
@@ -1,16 +1,34 @@
+"use client";
 import { Header } from "@/components/Header";
+import { MatchMainFilter } from "@/components/MatchMainFilter";
 import { Navbar } from "@/components/Navbar";
+import { useState } from "react";
 export default function Home() {
+  const [mainFilter, setMainFilter] = useState("upcomming");
+  const [filter, setFilter] = useState("upcomming");
+
+  const handdleChangeMainFilter = (text: string) => {
+    setMainFilter(text);
+    setFilter("");
+  };
+
   return (
     <div className="flex flex-col items-center justify-start space-y-4 h-screen w-screen text-white">
       <div className="relative m-0 p-0 top-0 flex flex-col w-full">
         <Header />
         <Navbar pagenow="match" />
       </div>
-      <p>
-        ดูและทายผลการแข่งกีฬา intania game ฟรี! เว็บเดียวในวิศวะจุฬา
-        แชร์กันเยอะๆ
-      </p>
+
+      <div className="w-[95%] sm:w-[700px] items-center flex flex-col space-y-4">
+        <p className="text-center max-w-[70vw] sm:hidden text-sm">
+          ดูและทายผลการแข่งกีฬา intania game ฟรี! เว็บเดียวในวิศวะจุฬา
+          แชร์กันเยอะๆ
+        </p>
+        <MatchMainFilter
+          mainFilter={mainFilter}
+          handdleChangeMainFilter={handdleChangeMainFilter}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/MatchMainFilter.tsx
@@ -0,0 +1,39 @@
+export const MatchMainFilter = (props: {
+  mainFilter: string;
+  handdleChangeMainFilter: (text: string) => void;
+}) => {
+  return (
+    <div className="w-full flex flex-row space-x-2 sm:space-x-4 items-center justify-center">
+      <button
+        className={`${
+          props.mainFilter === "upcomming"
+            ? "bg-neutral-700 text-white"
+            : "bg-neutral-800 text-neutral-300 "
+        } flex items-center justify-center h-10 w-40 sm:h-14 text-[0.8rem] sm:text-md sm:w-48 rounded-lg font-semibold`}
+        onClick={() => props.handdleChangeMainFilter("upcomming")}
+      >
+        ตารางการแข่งขัน
+      </button>
+      <button
+        className={`${
+          props.mainFilter === "result"
+            ? "bg-neutral-700 text-white"
+            : "bg-neutral-800 text-neutral-300 "
+        } flex items-center justify-center h-10 w-40 sm:h-14 text-[0.8rem] sm:text-md sm:w-48 rounded-lg font-semibold`}
+        onClick={() => props.handdleChangeMainFilter("result")}
+      >
+        ผลการแข่งขัน
+      </button>
+      <button
+        className={`${
+          props.mainFilter === "overall"
+            ? "bg-neutral-700 text-white"
+            : "bg-neutral-800 text-neutral-300 "
+        } flex items-center justify-center h-10 w-40 sm:h-14 text-[0.8rem] sm:text-md sm:w-48 rounded-lg font-semibold`}
+        onClick={() => props.handdleChangeMainFilter("overall")}
+      >
+        ภาพรวม
+      </button>
+    </div>
+  );
+};
