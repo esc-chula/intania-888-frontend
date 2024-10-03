aec83e724098732634409027275499c250f0042c
neennera <neennera@gmail.com>
2024-10-03 14:26:22 +0700

    add: header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Playground from "@/components/Playground";
+import { Header } from "@/components/Header";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center space-y-4 h-screen">
+      <Header />
       <Playground />
     </div>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,40 @@
+import { Intania888Logo } from "../../public/logos/Intania888-logo";
+import Image from "next/image";
+export const Header = () => {
+  return (
+    <div className="absolute top-0 w-screen h-28 p-2 md:py-2 md:px-12 flex flex-row justify-between items-center bg-neutral-900">
+      <div className="sm:w-1/4 sm:min-w-[300px] w-[160px]">
+        <Intania888Logo />
+      </div>
+
+      <div className="flex-col hidden lg:block space-y-1 w-1/2 text-white font-semibold">
+        <p>ดูและทายผลการแข่งกีฬา intania game ฟรี!</p>
+        <p>เว็บเดียวในวิศวะจุฬา แชร์กันเยอะๆ</p>
+      </div>
+
+      <div className="flex flex-col items-center justify-center max-sm:w-1/2 text-white space-y-2 text-normal max-sm:text-[0.7rem]">
+        <a
+          target="_blank"
+          href="https://www.instagram.com/intania.games/"
+          className="bg-[#FE0966] items-center justify-start w-44 sm:w-64 px-2 sm:px-4 font-semibold rounded-lg flex flex-row space-x-2 sm:space-x-5 py-1"
+        >
+          <Image src="/logos/IgLogo.png" alt="ig_logo" height={30} width={30} />
+          <p>@intania.games</p>
+        </a>
+        <a
+          target="_blank"
+          href="https://www.instagram.com/intania.games/"
+          className="bg-[#00C300] items-center justify-start w-44 sm:w-64 px-2 sm:px-4 font-semibold rounded-lg flex flex-row space-x-2 sm:space-x-5 py-1"
+        >
+          <Image
+            src="/logos/LineLogo.png"
+            alt="ig_logo"
+            height={30}
+            width={30}
+          />
+          <p>เข้า openchat สีเลย!</p>
+        </a>
+      </div>
+    </div>
+  );
+};
