79cec1a9d0b401e2c18de16e185122462075f399
neennera <neennera@gmail.com>
2024-10-09 00:56:03 +0700

    add: get color leaderboard utils

---
+++ src/api/match/getColorLeaderboard.ts
@@ -0,0 +1,30 @@
+import { AxiosResponse } from "axios";
+import { apiClient } from "../axios";
+
+
+const getColorLeaderboard = async (props: {type_id : string}) => {
+    try {
+        const response: AxiosResponse = await apiClient.get("/colors/leaderboards",
+            {
+                params:{
+                    type_id : props.type_id
+                }
+            }
+        )
+        
+        if (response.status == 200) {
+            const data = response.data;
+            console.log(data);
+            
+            return { success: true, data :  data }
+        } else {
+            return { success: false }
+        }
+        
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+
+export { getColorLeaderboard }
