76a4dee77c3a3ca86eee4ea7a4e8ea492c1d489e
neennera <neennera@gmail.com>
2024-10-09 01:00:27 +0700

    fix: I messup a coin & sport leaderboard

--- src/components/LeaderBoardTable.tsx
+++ src/components/LeaderBoardTable.tsx
@@ -1,19 +1,33 @@
-import { randomInt } from "crypto";
-import { Coins } from "lucide-react";
+import { getColorLeaderboard } from "@/api/match/getColorLeaderboard";
 
-export const CoinLeaderBoardTable = () => {
+export const CoinLeaderBoardTable = (props: { sport: string }) => {
+  const Data = getColorLeaderboard({
+    type_id: props.sport === "รวมกีฬาทุกประเภท" ? "" : props.sport,
+  });
+
+  console.log(Data);
+
   return (
     <table className="rounded-lg w-[90vw] sm:w-[600px] overflow-hidden text-[0.8rem] sm:text-[1rem] h-auto">
       <thead className="bg-[#4E0F15] font-semibold h-12 flex flex-row">
         <tr className="w-full flex flex-row">
-          <td className="flex items-center justify-center w-[15%] h-full">
+          <td className="flex items-center justify-center w-[10%] h-full">
             ลำดับ
           </td>
-          <td className="flex items-center justify-start w-[45%] sm:w-[55%] h-full ">
-            ชื่อ
+          <td className="flex items-center justify-start w-[20%] h-full ">
+            สี
           </td>
-          <td className="flex flex-row space-x-2 items-center justify-end pr-2 sm:pr-10 w-[40%] sm:w-[30%] h-full ">
-            <p>จำนวนเหรียญ</p> <Coins color="yellow" />
+          <td className="flex items-center justify-start w-[20%] h-full ">
+            กรุ๊ป
+          </td>
+          <td className="flex flex-row space-x-2 items-center justify-end pr-2 sm:pr-10 w-[16%] h-full ">
+            1st 🏆
+          </td>
+          <td className="flex flex-row space-x-2 items-center justify-end pr-2 sm:pr-10 w-[16%] h-full ">
+            2nd 🥈
+          </td>
+          <td className="flex flex-row space-x-2 items-center justify-end pr-2 sm:pr-10 w-[16%] h-full ">
+            3-4th 🥉
           </td>
         </tr>
       </thead>
@@ -24,34 +38,28 @@ export const CoinLeaderBoardTable = () => {
               key={index}
               className="text-black w-full bg-white font-semibold h-12 flex flex-row border-y-[0.5px]"
             >
-              <td className="flex items-center justify-center w-[15%] h-full ">
+              <td className="flex items-center justify-center w-[10%] h-full ">
                 {index + 1}
               </td>
-              <td className="flex items-center justify-start w-[55%] h-full ">
+              <td className="flex items-center justify-start w-[20%] h-full ">
                 <NameAndColor name={item.name} color={item.color} />
               </td>
+              <td className="flex items-center justify-start w-[20%] h-full ">
+                {groupColor[item.color]}
+              </td>
               <td className="flex flex-row space-x-2 items-center justify-end pr-10 sm:pr-20 w-[30%] h-full ">
-                <p>
-                  {index * 15000 + randomInt(1000)}.{randomInt(100)}
-                </p>
+                {10}
+              </td>
+              <td className="flex flex-row space-x-2 items-center justify-end pr-10 sm:pr-20 w-[30%] h-full ">
+                {0}
+              </td>
+              <td className="flex flex-row space-x-2 items-center justify-end pr-10 sm:pr-20 w-[30%] h-full ">
+                {0}
               </td>
             </tr>
           );
         })}
       </tbody>
-      <tfoot className="font-semibold bg-neutral-200 text-black h-12 flex flex-row">
-        <tr className="w-full flex flex-row">
-          <td className="flex items-center justify-center w-[15%] h-full ">
-            157
-          </td>
-          <td className="flex items-center justify-start w-[55%] h-full ">
-            <NameAndColor name="เจ้าหมู" color="pink" />
-          </td>
-          <td className="flex flex-row space-x-2 items-center justify-end pr-10 sm:pr-20 w-[30%] h-full ">
-            15000.00
-          </td>
-        </tr>
-      </tfoot>
     </table>
   );
 };
@@ -69,38 +77,46 @@ const Top10 = [
   { name: "เจ้าเจ้า", color: "yellow" },
 ];
 
+const groupColor: { [key: string]: string } = {
+  PINK: "[E,L,P]",
+  ORANGE: "[J,K,M]",
+  GREEN: "[H,S,T]",
+  BLUE: "[Dog,N,R]",
+  VIOLET: "[A,C,F]",
+  YELLOW: "[B,G,Q]",
+};
 const NameAndColor = (props: { name: string; color: string }) => {
-  if (props.color == "violet")
+  if (props.color == "VIOLET")
     return (
       <p>
         {props.name} <span className="text-team-violet">สีม่วง </span>
       </p>
     );
-  if (props.color == "blue")
+  if (props.color == "BLUE")
     return (
       <p>
         {props.name} <span className="text-team-blue">สีฟ้า</span>
       </p>
     );
-  if (props.color == "yellow")
+  if (props.color == "YELLOW")
     return (
       <p>
         {props.name} <span className="text-team-yellow">สีเหลือง</span>
       </p>
     );
-  if (props.color == "green")
+  if (props.color == "GREEN")
     return (
       <p>
         {props.name} <span className="text-team-green">สีเขียว</span>
       </p>
     );
-  if (props.color == "pink")
+  if (props.color == "PINK")
     return (
       <p>
         {props.name} <span className="text-team-pink">สีชมพู</span>
       </p>
     );
-  if (props.color == "orange")
+  if (props.color == "ORANGE")
     return (
       <p>
         {props.name} <span className="text-team-orange">สีส้ม</span>
