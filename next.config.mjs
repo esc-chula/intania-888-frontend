be56e455b9c1c40d3554cab78e2b36e7ed823e77
Sirawit Chanaburanasak <ext.sirawit@skooldio.com>
2024-10-11 03:20:11 +0700

    cannot parsed env

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     compress: true,
     output: 'standalone',
     env: {
-        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL ,
+        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://888api.chula.engineering/v1/api' ,
     },
 };
 
