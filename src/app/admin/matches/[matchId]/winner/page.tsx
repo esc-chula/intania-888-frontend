d72ec2995e2558d903821080c0fc95818656c3f7
Sirawit Chanaburanasak <ext.sirawit@skooldio.com>
2024-10-10 23:26:51 +0700

    feat: add draw as a option

--- src/app/admin/matches/[matchId]/winner/page.tsx
+++ src/app/admin/matches/[matchId]/winner/page.tsx
@@ -1,26 +1,49 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter, useParams } from "next/navigation";
 import { apiClient } from "@/api/axios";
 
 const UpdateWinner = () => {
   const [winner, setWinner] = useState<string>("");
+  const [teamAName, setTeamAName] = useState<string>("");
+  const [teamBName, setTeamBName] = useState<string>("");
+
   const { matchId } = useParams();
   const router = useRouter();
 
+  useEffect(() => {
+    const fetchMatch = async () => {
+      try {
+        const res = await apiClient.get(`/matches/${matchId}`);
+        setTeamAName(res.data.team_a);
+        setTeamBName(res.data.team_b);
+      } catch (error) {
+        console.error("Error fetching match:", error);
+      }
+    };
+
+    fetchMatch();
+  }, [matchId]);
+
   const handleUpdateWinner = async () => {
     try {
-      await apiClient.patch(`/matches/${matchId}/winner/${winner}`);
-      alert("Winner updated successfully!");
+      if (winner === "draw") {
+        // Call the API for marking the match as a draw
+        await apiClient.patch(`/matches/${matchId}/draw`);
+      } else {
+        // Call the API for updating the winner
+        await apiClient.patch(`/matches/${matchId}/winner/${winner}`);
+      }
+      alert("Match result updated successfully!");
       router.push("/admin");
     } catch (error) {
-      console.error("Error updating winner:", error);
+      console.error("Error updating match result:", error);
     }
   };
 
   return (
     <div className="p-4 text-white">
-      <h1 className="text-xl font-bold mb-4">Update Match Winner</h1>
+      <h1 className="text-xl font-bold mb-4">Update Match Result</h1>
       <div className="space-y-2">
         <label>Choose the Winner</label>
         <select
@@ -28,15 +51,16 @@ const UpdateWinner = () => {
           onChange={(e) => setWinner(e.target.value)}
           className="border p-2 text-black"
         >
-          <option value="">Select Winner</option>
-          <option value="BLUE">Team Blue</option>
-          <option value="RED">Team Red</option>
+          <option value="">Select Result</option>
+          <option value={teamAName}>{teamAName}</option>
+          <option value={teamBName}>{teamBName}</option>
+          <option value="draw">Draw</option> {/* Add Draw option */}
         </select>
         <button
           className="px-4 py-2 bg-green-500 text-white"
           onClick={handleUpdateWinner}
         >
-          Update Winner
+          Update Result
         </button>
       </div>
     </div>
