8d5ed39716d43c2f57851b42ce462e4282d9db98
Sirawit Chanaburanasak <ext.sirawit@skooldio.com>
2024-10-11 03:07:50 +0700

    fix: parsed env

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     reactStrictMode: false,
     compress: true,
     output: 'standalone',
+    env: {
+        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL ,
+    },
 };
 
 export default nextConfig;
