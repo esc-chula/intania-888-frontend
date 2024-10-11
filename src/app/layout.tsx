9a952c7f94d9695fe932e297eb9cdf661535dd90
Sirawit Chanaburanasak <ext.sirawit@skooldio.com>
2024-10-11 20:23:48 +0700

    feat: add app icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icon.png" type="image/png" />
+      </head>
       <body
         className={`${IBMFont.className} antialiased bg-black`}
       >
