1abf413443d2c7fe909bab054ed6691c96995ec4
Sirawit Chanaburanasak <ext.sirawit@skooldio.com>
2024-10-11 17:19:04 +0700

    fix: env

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     compress: true,
     output: 'standalone',
     env: {
-        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://888api.chula.engineering/v1/api' ,
+        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://888api.chula.engineering/api/v1' ,
     },
 };
 
