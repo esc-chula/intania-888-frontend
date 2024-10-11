5675c132a9b8bc66d394d486dd92b55858d56cf9
Sirawit Chanaburanasak <ext.sirawit@skooldio.com>
2024-10-11 17:29:41 +0700

    remove console

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     reactStrictMode: false,
     compress: true,
     output: 'standalone',
+    compiler: {
+        removeConsole: true,
+      },
     env: {
         NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://888api.chula.engineering/api/v1' ,
     },
