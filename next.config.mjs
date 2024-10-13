48030b0b0dbf67a71fc7e833f6d518a023d54008
Sirawit Chanaburanasak <ext.sirawit@skooldio.com>
2024-10-13 18:34:39 +0700

    build: parsed `env` on build

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         removeConsole: true,
       },
     env: {
-        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://888api.chula.engineering/api/v1' ,
+        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL ,
     },
 };
 
