20cf6ccf764971ee7f5db452a6c1d12bcc0fc548
RawSalmon <phanthawasjira@Gmail.com>
2025-10-19 01:01:54 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <div className="fixed bottom-0 overflow-clip w-full flex justify-center cursor-pointer animate-blink z-10">
+    <div className="fixed bottom-0 overflow-clip w-full flex justify-center cursor-pointer animate-blink z-10 md:h-32">
       <Link
         href="https://docs.google.com/forms/d/1bq6gw44xRH98jXVkAk4u8svNsEBPmBtEvvdvkL4zpKQ"
         className="relative"
@@ -14,7 +14,7 @@ export const Footer = () => {
           alt="sweatshirt_banner"
           width={2214}
           height={330}
-          className="object-contain w-auto hover:scale-110 duration-500"
+          className="object-contain w-auto md:h-32 hover:scale-110 duration-500"
         />
       </Link>
     </div>
